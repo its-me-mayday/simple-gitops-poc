cee20fe644bb57bc768b12b0b94c358fff570dc3
Luca Maggio <lucamaggio1992@gmail.com>
2025-09-29 15:28:53 +0200

    build: add argocd-apps and setup all elements to provision in script

--- 00-provision-kind-shared.sh
+++ 00-provision-kind-shared.sh
@@ -2,6 +2,10 @@
 
 kind create cluster --name simple-gitops-poc --config ./cluster.kind.yaml
 helm upgrade --install my-argo argo/argocd --version 8.5.5  --values values.argocd.yaml --namespace argocd --create-namespace
+helm upgrade --install cnpg \                                                                                                
+  --namespace cnpg-system \
+  --create-namespace \
+  cnpg/cloudnative-pg
+kubectl create ns gitea
 kubectl apply -f cr.cluster.yaml --namespace gitea
 helm upgrade --install my-gitea gitea/gitea --version 12.3.0 --values values.gitea.yaml --namespace gitea --create-namespace
-
