9c863d709bd2ac6b71990f6febd173a10c8cd8e6
Luca Maggio <lucamaggio1992@gmail.com>
2025-09-29 13:34:01 +0200

    build(gitea): provision organization and repositories

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    gitea = {
+      source = "go-gitea/gitea"
+      version = "0.7.0"
+    }
+  }
+}
+
+provider "gitea" {
+  base_url = var.gitea_url
+  username = var.gitea_username
+  password = var.gitea_password
+  insecure = true 
+}
+
+resource "gitea_org" "test_org" {
+  name = "simple-gitops-poc"
+}
+
+resource "gitea_repository" "dev_repo" {
+  username = gitea_org.test_org.name
+  name = "cluster-dev-poc"
+}
+
+resource "gitea_repository" "staging_repo" {
+  username = gitea_org.test_org.name
+  name = "cluster-staging-poc"
+}
+
+resource "gitea_repository" "prod_repo" {
+  username = gitea_org.test_org.name
+  name = "cluster-prod-poc"
+}
